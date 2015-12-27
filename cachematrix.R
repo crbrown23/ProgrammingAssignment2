58b8f9ae0144f0cf68bbf4db93da76dfd743e230
crbrown23 <crbrown71@gmail.com>
2015-12-27 11:47:07 -0800

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 - Caching the Inverse of a Matrix
+## Notes:
+##   clarification https://github.com/DanieleP/PA2-clarifying_instructions
+##   <<- assigns globally
 
-## Write a short comment describing this function
+## makeCacheMatrix() creates special "matrix" object that can cache its inverse
+##    1) set the matrix values
+##    2) get the matrix
+##    3) set the values of inverse
+##    4) get the values of inverse
+##       this list is used as input for cacheSolve()
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- null
+        }
+        get <- function() x
+        setInv <- function(inverse) m <<- inverse
+        getInv <- function() m
+        list(set = set, get = get, 
+             setInv = setInv,
+             getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() computes the inverse of the makceCacheMatrix list. 
+##              if the inverse already calculed & matrix not changed
+##              then retrieve inverse in cache
+##    1) get inverse matrix list
+##    2) if already calculated, skip calculations and pull in cache data
+##    3) if not already calculated, run calculation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached inverse data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m
 }
+
+## unit test
